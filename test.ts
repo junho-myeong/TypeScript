915d938987b59cf6dc433b72b015b5b7e4e9790e
junho-myeong <wnsgh9166@naver.com>
2022-07-26 15:22:02 +0900

    type script setting, typescript annotation

---
+++ test.ts
@@ -0,0 +1,13 @@
+// First Type Annotation
+// let a = 'Mark' // tpyescript에서는 a라는 타입은 문자열이라고 지정되어 버린다.
+// a = 39 // tpye script는 재할당 과정에서 데이터의 타입이 다르면 재할당이 불가능하다.
+
+// let b: number // 자바 처럼 변수에 특정 타입을 지정 해주는 일을 Type Annotation이라고 부른다.
+// b = 'mark'
+// b = 37
+
+// function hello(b: number) {
+//   console.log(b)
+// }
+// hello(1)
+// hello('a')
